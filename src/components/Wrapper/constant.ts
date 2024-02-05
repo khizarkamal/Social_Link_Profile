54f351f98873e11977c7301858a1ec22fe0e1bad
Khizar Kamal <khizarkamal0321@gmail.com>
2024-02-06 00:27:35 +0500

    fix: Added account urls

--- src/components/Wrapper/constant.ts
+++ src/components/Wrapper/constant.ts
@@ -2,17 +2,17 @@ export const porfolios = [
   {
     id: "github",
     text: "Github",
-    url: "",
+    url: "https://github.com/khizarkamal",
     // icon: <FaGithub></FaGithub>,
   },
   {
     id: "linkedin",
     text: "Linkedin",
-    url: "",
+    url: "https://www.linkedin.com/in/khizar-kamal-269b67170/",
   },
   {
     id: "instagram",
     text: "Instagram",
-    url: "",
+    url: "/",
   },
 ];
